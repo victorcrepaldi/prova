3e77395e19552bae294e81d5eec86abb992b58f4
victor.crepaldi <victor_primos@hotmail.com>
2015-03-12 21:27:40 -0300

    corrigindo disciplina.js

--- Trabalho/src/main/webapp/pages/disciplinas/disciplina.js
+++ Trabalho/src/main/webapp/pages/disciplinas/disciplina.js
@@ -15,17 +15,43 @@ function DisciplinaController($scope,$http) {
     $scope.isNovo = true;
     
     function funcaoEditar(aluno) {
+        $http.get("/disciplinas").success(onSuccess).error(onError);
+        
         $scope.disciplina = angular.copy(aluno);
         $scope.isNovo = false;
     }
 
     
     function funcaoExcluir(aluno) {
+        $http.delete("/disciplinas" + id ).success(onSuccess).error(onError);
         
+        function onSuccess(data, status) {
+            funcaoCarregar();
+            $scope.disciplinas = data;       
+           
+        }
+        function onError(data, status) {
+            alert("Deu erro: " + data);
+        }
     }
     
     function funcaoSalvar() {
+        if($scope.novo){
+            $http.post("/disciplinas").success(onSuccess).error(onError);
+        }else{
+            $http.put("/disciplinas").success(onSuccess).error(onError);
+        }
         
+        function onSuccess(data, status) {
+            funcaoCarregar();
+            $scope.disciplina = {};
+            $scope.isNovo = true;
+                  
+           
+        }
+        function onError(data, status) {
+            alert("Deu erro: " + data);
+        }
     }
     
     function funcaoCarregar() {
